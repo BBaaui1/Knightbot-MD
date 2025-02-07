d6d6e160752e864cdd8feb73a16dc9c5573c24b2
BBaaui1 <bettysylvain648@gmail.com>
2025-02-06 20:51:36 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Mr-Andromède', // Your name
+  ownerNumber: '50940993901', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
